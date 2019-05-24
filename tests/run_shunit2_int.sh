d69fb21abd3379eb813fb1bb7abaacad2441336c
Ulrich Berntien <44895863+UlrichBerntien@users.noreply.github.com>
2019-05-24 12:44:47 +0200

    more tests
    * added a simple test for the most public functions: function must run without error message output.
    * added some tests of internal functions
    * changed some alias definition to function definition to test the functions
    * support grep tools without --color switch and also grep tools needed the --color switch
    * small bug fixes and some better quotations in Orc

--- tests/run_shunit2_int.sh
+++ tests/run_shunit2_int.sh
@@ -27,8 +27,190 @@ test_orc_noop () {
   output=$(orc_noop)
   assertEquals 'returned false' 0 $?
   assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
   error=$(orc_noop 2>&1 > /dev/null)
   assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+}
+
+
+test_orc_colorNever () {
+  # Test the orc_colorNever flag for the grep tool
+  # The orc_colorNever variable is defined in the o.rc.
+  # So the shellcheck 2154 must be disabled.
+  # The varaible must not used with quotes because the
+  # empty string must not used as argument.
+  # So the shellcheck 2086 must be disabled.
+  # shellcheck disable=SC2154,SC2086
+  output=$(echo 'test' | grep $orc_colorNever 'no' 2>&1)
+  assertNotEquals 'returned not false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+  # shellcheck disable=SC2154,SC2086
+  output=$(echo 'test' | grep $orc_colorNever 'test' 2>&1)
+  assertEquals 'returned false' 0 $?
+  assertNotNull 'output is null' "$output"
+  assertEquals 'returned not grep' 'test' "$output"
+  # shellcheck disable=SC2154,SC2086
+  output=$(echo 'test' | grep $orc_colorNeveri -q 'no' 2>&1)
+  assertNotEquals 'returned not false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+  # shellcheck disable=SC2154,SC2086
+  output=$(echo 'test' | grep $orc_colorNever -q 'test' 2>&1)
+  assertEquals 'returned false' 0 $?
+  assertNull 'output is not null' "$output"
+  output=$(echo 'test' | grep -q 'no' 2>&1)
+  assertNotEquals 'returned not false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+  output=$(echo 'test' | grep -q 'test' 2>&1)
+  assertEquals 'returned false' 0 $?
+  assertNull 'output is not null' "$output"
+}
+
+
+test_orc_existsProg () {
+  # Test the orc_existsProg function
+  output=$(orc_existsProg orc_noop 2>&1)
+  assertEquals 'returned false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+  output=$(orc_existsProg ' this is not a program ' 2>&1)
+  assertNotEquals 'returned not false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+}
+
+
+test_orc_listArp () {
+  # Test the orc_listArp function
+  output=$(orc_listArp)
+  assertEquals 'returned false' 0 $?
+  assertNotNull 'output is null' "$output"
+  # One address = one word per line
+  assertEquals 'lines and words' "$(echo "$output"|wc -l)" "$(echo "$output"|wc -w)"
+  error=$(orc_listArp 2>&1 > /dev/null)
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+}
+
+
+test_orc_listBroadcastAddress () {
+  # Test the orc_listBroadcastAddress function
+  output=$(orc_listBroadcastAddress)
+  assertEquals 'returned false' 0 $?
+  assertNotNull 'output is null' "$output"
+  # One address = one word per line
+  assertEquals 'lines and words' "$(echo "$output"|wc -l)" "$(echo "$output"|wc -w)"
+  error=$(orc_listBroadcastAddress 2>&1 > /dev/null)
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+}
+
+
+test_orc_inetAddressAndMask () {
+  # Test the orc_inetAddressAndMask function
+  output=$(orc_inetAddressAndMask)
+  assertEquals 'returned false' 0 $?
+  assertNotNull 'output is null' "$output"
+  # One address plus mask per line = two words per line
+  assertEquals 'lines and words' "$(( $(echo "$output"|wc -l) *2))" "$(echo "$output"|wc -w)"
+  error=$(orc_inetAddressAndMask 2>&1 > /dev/null)
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+}
+
+
+test_orc_exportProxySettings () {
+  # Test the orc_exportProxySettings function
+  output=$(orc_exportProxySettings 2>&1)
+  assertEquals 'returned false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+  http_proxy='http-test'
+  https_proxy='https-test'
+  output=$(orc_exportProxySettings 2>&1)
+  orc_exportProxySettings
+  assertEquals 'returned false' 0 $?
+  assertEquals 'http_proxy 1' 'http-test' "$(sh -c 'echo $http_proxy')"
+  assertEquals 'http_proxy 2' 'http-test' "$(sh -c 'echo $HTTP_PROXY')"
+  assertEquals 'https_proxy 1' 'https-test' "$(sh -c 'echo $https_proxy')"
+  assertEquals 'https_proxy 2' 'https-test' "$(sh -c 'echo $HTTPS_PROXY')"
+  http_proxy=''
+  HTTP_PROXY='http2-test'
+  https_proxy=''
+  HTTPS_PROXY='https2-test'
+  orc_exportProxySettings
+  assertEquals 'returned false' 0 $?
+  assertEquals 'http_proxy 3' 'http2-test' "$(sh -c 'echo $http_proxy')"
+  assertEquals 'http_proxy 4' 'http2-test' "$(sh -c 'echo $HTTP_PROXY')"
+  assertEquals 'https_proxy 3' 'https2-test' "$(sh -c 'echo $https_proxy')"
+  assertEquals 'https_proxy 4' 'https2-test' "$(sh -c 'echo $HTTPS_PROXY')"
+  assertEquals 'http_proxy 5' 'http2-test' "$http_proxy"
+  assertEquals 'http_proxy 6' 'http2-test' "$HTTP_PROXY"
+  assertEquals 'https_proxy 5' 'https2-test' "$https_proxy"
+  assertEquals 'https_proxy 6' 'https2-test' "$HTTPS_PROXY"
+  # clear http proxy settings for follwing tests
+  http_proxy=''
+  HTTP_PROXY=''
+  https_proxy=''
+  HTTPS_PROXY=''
+}
+
+
+test_orc_loadURL () {
+  # Test the orc_loadURL function
+  output=$(orc_loadURL https://raw.githubusercontent.com/zMarch/Orc/master/resources/echo_arguments.sh)
+  assertEquals 'returned false' 0 $?
+  assertNotNull 'output is null' "$output"
+  assertTrue 'less than 10 lines' "[ $(echo "$output"|wc -l) -ge 10 ]"
+  assertTrue 'less than 50 words' "[ $(echo "$output"|wc -w) -ge 50 ]"
+  assertContains 'in download' "$output" '#!'
+  assertContains 'in download' "$output" 'echo'
+  error=$(orc_loadURL https://raw.githubusercontent.com/zMarch/Orc/master/resources/echo_arguments.sh 2>&1 > /dev/null)
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+}
+
+
+test_orc_tryTcpConnection () {
+  # Test the orc_tryTcpConnection function
+  output=$(orc_tryTcpConnection 'raw.githubusercontent.com' 80 2>&1)
+  assertEquals 'returned false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+  output=$(orc_tryTcpConnection 'raw.githubusercontent.com' 43 2>&1)
+  assertNotEquals 'returned not false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+}
+
+
+test_orc_listtmp () {
+  # Test the orc_listtmp function
+  output=$(orc_listtmp)
+  assertEquals 'returned false' 0 $?
+  assertNotNull 'output is null' "$output"
+  assertTrue 'less than 3 lines' "[ $(echo "$output"|wc -l) -ge 3 ]"
+  echo "$output" |
+  while read -r t; do
+    assertTrue 'not directory' "[ -d $t ]"
+  done
+  error=$(orc_listtmp 2>&1 > /dev/null)
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+}
+
+
+test_orc_makeHome () {
+  # Test the orc_makeHome function
+  output=$(orc_makeHome 2>&1)
+  assertEquals 'returned false' 0 $?
+  assertNull 'output is not null' "$output"
+  if [ -n "$output" ]; then echo "$output"; fi
+  assertNotNull 'HOME' "$HOME"
+  assertTrue 'not directory' "[ -d $HOME ]"
 }
 
 
--- tests/run_shunit2_user.sh
+++ tests/run_shunit2_user.sh
@@ -31,13 +31,15 @@ test_gethelp() {
   assertTrue 'less than 5 lines' "[ $(echo "$output"|wc -l) -ge 5 ]"
   error=$(gethelp 2>&1 > /dev/null)
   assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
 }
 
 
 test_getsfiles() {
-  # Tets the getsfiles function
+  # Test the getsfiles function
   error=$(getsfiles background 2>&1 > /dev/null)
   assertNull 'error message in background mode' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
   output=$(getsfiles 2> /dev/null)
   assertEquals 'returned false' 0 $?
   assertNotNull 'output is null' "$output"
@@ -45,9 +47,237 @@ test_getsfiles() {
   assertTrue 'less than 5 lines' "[ $(echo "$output"|wc -l) -ge 5 ]"
   error=$(getsfiles 2>&1 > /dev/null)
   assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
   assertTrue 'missing sfiles' "[ -f sfiles ]"
   assertTrue 'less than 5 lines in sfiles' "[ $(wc -l < sfiles) -ge 5 ]"
   rm sfiles
+}
+
+
+test_dropsuid() {
+  # Test the dropsuid function
+  error=$(dropsuid 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+#test_getdbus() {
+# Run with Travis CI raises the error:
+# Failed to open connection to "session" message bus: Unable to autolaunch a dbus-daemon without a $DISPLAY for X11
+#  # Test the getdbus function
+#  error=$(getdbus 2>&1 > /dev/null)
+#  assertEquals 'returned false' 0 $?
+#  assertNull 'error message' "$error"
+#  if [ -n "$error" ]; then echo "$error"; fi
+#  # TODO: add more checks
+#}
+
+
+test_getdocker() {
+  # Test the getdocker function
+  error=$(getdocker 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getenum() {
+  # Test the getenum function
+  error=$(getenum 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getescape() {
+  # Test the getescape function
+  error=$(getescape 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getexploit() {
+  # Test the getexploit function
+  error=$(getexploit 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getidle() {
+  # Test the getidle function
+  error=$(getidle 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+#test_getinfo() {
+#  # Test the getinfo function
+#  error=$(getinfo 2>&1 > /dev/null)
+#  assertEquals 'returned false' 0 $?
+# TODO: suppress message "Removing leading '/' from member names
+#  assertNull 'error message' "$error"
+#  if [ -n "$error" ]; then echo "$error"; fi
+#  # TODO: add more checks
+#}
+
+
+test_getip() {
+  # Test the getip function
+  error=$(getip 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getjail() {
+  # Test the getjail function
+  error=$(getjail 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getluks() {
+  # Test the getluks function
+  error=$(getluks 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+#test_getnet() {
+#  # Test the getnet function
+#  error=$(getnet 2>&1 > /dev/null)
+#  assertEquals 'returned false' 0 $?
+# TODO: suppress message "Removing leading '/' from member names
+#  assertNull 'error message' "$error"
+#  if [ -n "$error" ]; then echo "$error"; fi
+#  # TODO: add more checks
+#}
+
+
+test_getrel() {
+  # Test the getrel function
+  error=$(getrel 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getsec() {
+  # Test the getsec function
+  error=$(getsec 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getspec() {
+  # Test the getspec function
+  error=$(getspec 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_gettmp() {
+  # Test the gettmp function
+  error=$(gettmp 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getusers() {
+  # Test the getusers function
+  error=$(getusers 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_getuservices() {
+  # Test the getuservices function
+  error=$(getuservices 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_portscan() {
+  # Test the portscan function
+  error=$(portscan localhost 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+test_prochide() {
+  # Test the prochide function
+  error=$(prochide 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
+}
+
+
+#test_srm() {
+#  # Test the srm function
+#  echo "test file for srm" > test_file_for_srm
+#  error=$(srm test_file_for_srm 2>&1 > /dev/null)
+#  assertEquals 'returned false' 0 $?
+# TODO: no error output if srm works well.
+#  assertNull 'error message' "$error"
+#  if [ -n "$error" ]; then echo "$error"; fi
+#  assertFalse 'test file not removed' "[ -e test_file_for_srm ]"
+#}
+
+
+test_tools() {
+  # Test the tools function
+  error=$(tools 2>&1 > /dev/null)
+  assertEquals 'returned false' 0 $?
+  # Some missing tools are normal. So $error could be contain messages
+  # assertNull 'error message' "$error"
+  if [ -n "$error" ]; then echo "$error"; fi
+  # TODO: add more checks
 }
 
 
